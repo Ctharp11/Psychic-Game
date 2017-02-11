1aba46f85cad29f30d076f49cde1673ade3d17b3
Ctharp11 <ctharp1988@gmail.com>
2017-02-10 18:20:36 -0600

    updated css and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,55 @@
-
-
-//computer chooses random number from array and console.logs it 
-
+//computer chooses random number from array and console.logs it
 var computerChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 var computer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 console.log(computer);
+//variables for wins and losses.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guesses = guessesLeft;
 
-
-//variables for wins and losses.  
-
-var wins = 1; 
-
-var losses = 1; 
-
-var guesses = 9;
-
-
-//user choice array 
-
+//user choice array
 var userChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-
 //function that places userchoice into #left
 
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "Losses: " + losses;
+document.getElementById('left').innerHTML = ("Guesses Left: " + guesses);
+
+
 document.onkeypress = function(event) {
-
-   var userChoice = event.key;
-   var newEl = document.createElement('span');
-   var newContent = document.createTextNode(userChoice + ', ');
-   newEl.appendChild(newContent);
-   var position = document.getElementById('guess');
-   position.appendChild(newEl);
-   
-
-//if else alerting win or lose 
-
-
-if (computer === userChoice) {
-   document.getElementById('wins').innerHTML = ("Wins: " + wins++);
-	 computer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-console.log(computer);
-   alert("You win! See if you can guess a new letter!")
-} else { 
-	document.getElementById('losses').innerHTML = ("Losses: " + losses++);
-  document.getElementById('left').innerHTML = ("Guesses Left: " + guesses--);
-     }  
+  var userChoice = event.key;
+ 
     
+  if (guesses === guessesLeft) {
+    document.getElementById('guess').innerText = "";
+    var newContent = document.createTextNode(userChoice); 
+  } else {
+    var newContent = document.createTextNode(', ' + userChoice);
+  }
+  var newEl = document.createElement('span').appendChild(newContent);
+  document.getElementById('guess').appendChild(newEl);
+
+//if first-child add coma, if not first-child don't add coma
+
+
+
+//if else alerting win or lose
+  if (computer === userChoice) {
+    guesses = 10;
+    wins++;
+    document.getElementById('wins').innerHTML = ("Wins: " + wins);
+    computer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    console.log(computer);
+    } else {
+    document.getElementById('left').innerHTML = ("Guesses Left: " + guesses--);
+  }
+  if (guesses === 0) {
+      losses++;
+      document.getElementById('losses').innerHTML = ("Losses: " + losses);
+      guesses = guessesLeft;
+
+    } 
+
 };
-
-if (guesses === 0) {
-  alert ('Game over!');
-};
-
-
-
-
-
-
 
